315d7e1ceccb319b9b9a7d1ef7a5a9588b466129
Patricio <pflood@frba.utn.edu.ar>
2022-04-22 22:18:43 -0300

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2022': true,
+    'node': true,
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+  }
+}
