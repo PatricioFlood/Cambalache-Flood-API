4ccac9981d53050c06ada36c4f593a14fce4eee7
Patricio <pflood@frba.utn.edu.ar>
2022-04-24 13:54:02 -0300

    added node_env

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const config = {
+  NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
